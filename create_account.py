3c415e514153eee05b015dc9bd2b54f9233202c5
Farrukh <farruh1607@mail.ru>
2020-02-04 23:54:57 +0500

    selenium commit

--- create_account.py
+++ create_account.py
@@ -17,6 +17,13 @@ passwd = driver.find_element_by_id('passp-field-passwd')
 passwd.send_keys('123qwe!@#')
 submit2 = driver.find_element_by_xpath("//button[@class='control button2 button2_view_classic button2_size_l button2_theme_action button2_width_max button2_type_submit passp-form-button']")
 submit2.click()
+time.sleep(5)
+
+add = driver.find_element_by_xpath("//button[@class='ui-flat-button ui-flat-button__width_available plus-button']")
+add.click()
+
+add_user = driver.find_element_by_class_name('menu-item menu-item_theme_islands i-bem menu-item_js_inited')
+
 
 if __name__ == '__main__':
     main()
