f41bda0d7fae9e35e84a9e324494ec23e3ce4707
Farrukh <farruh1607@mail.ru>
2020-02-04 23:35:02 +0500

    creating almost ready

--- create_account.py
+++ create_account.py
@@ -1,9 +1,22 @@
+import time
 from selenium import webdriver
 from excel_import import ExpressCreating
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
 
-driver.get('https://passport.yandex.uz/auth?from=mail&origin=hostroot_homer_auth_uz&retpath=https%3A%2F%2Fmail.yandex.uz%2F&backpath=https%3A%2F%2Fmail.yandex.uz%3Fnoretpath%3D1')
+driver.get('https://connect.yandex.com/portal/admin')
+
+login = driver.find_element_by_name('login')
+login.send_keys('ffazildjanov@texnomart.uz')
+submit1 = driver.find_element_by_xpath("//button[@class='control button2 button2_view_classic button2_size_l button2_theme_action button2_width_max button2_type_submit passp-form-button']")
+submit1.click()
+time.sleep(1)
+
+passwd = driver.find_element_by_id('passp-field-passwd')
+passwd.send_keys('123qwe!@#')
+submit2 = driver.find_element_by_xpath("//button[@class='control button2 button2_view_classic button2_size_l button2_theme_action button2_width_max button2_type_submit passp-form-button']")
+submit2.click()
 
 if __name__ == '__main__':
     main()
